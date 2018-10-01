39d00712e5e4030539e2d6ba208ffc66e5e05f45
amirdehghanfar <amirdehghanfar@gmail.com>
2018-10-01 21:19:20 +0330

    add current patient

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
 
     console.log("App started");
 
+    $('body').fadeIn(1000).removeClass('hidden');
+
     $("#currentPatientTable tr").click(function () {
-        alert("aa");
-        $.mobile.changePage("Patient.html",{transition:"slide"});
-        //window.location.href = "Patient.Html"
+        
+        window.location.href = "Patient.Html"
     });
 
     $(".chosen-select").chosen({ no_results_text: "Oops, nothing found!", width: "100%" });
